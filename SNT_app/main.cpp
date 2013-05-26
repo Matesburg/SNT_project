36cdfc1a61a051cca6e4d1208abfd77fab456b01
Matesburg <hacyn@seznam.cz>
2013-05-26 15:32:48 +0200

    Init NB projects

---
+++ SNT_app/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Matron
+ *
+ * Created on 26. květen 2013, 15:27
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
